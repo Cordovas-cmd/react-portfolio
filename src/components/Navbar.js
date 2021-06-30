2b02b83503deaf2db1df4ad891f3ca16b0cf7dc2
Samuel Cordova <cordovas1337@gmail.com>
2021-06-29 22:02:37 -0500

    pretty much doen with navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,40 @@
 import React from 'react'
 
+// Font awesome---------------------------------
+
+// import FontAwesome from 'react-fontawesome'
+// import faStyles from 'font-awesome/css/font-awesome.css'
+
+
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-            
-            </ul>
-           </div>
+
+            <a className="navbar-brand" href="#">Samuel Cordova</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse flex-row-reverse" id="navbarNavDropdown">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#about">about.me</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#about">services</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#portfolio">portfolio</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" aria-current="page" href="#contact">contact</a>
+                    </li>
+                    </ul>
+            </div>
         </div>
-      </nav>
+    </nav>
     )
 }
 
