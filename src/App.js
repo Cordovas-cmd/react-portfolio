47f2e1360ab05e2b60bb988aeb8eb0656d2235d8
Samuel Cordova <cordovas1337@gmail.com>
2021-06-30 00:10:35 -0500

    figured out the particles on the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar'
-import Header from './components/Header'
+import Particles from 'react-particles-js';
+// import Particles from "react-tsparticles";
+import Navbar from './components/Navbar';
+import Header from './components/Header';
 function App() {
   return (
     <>
+    <Particles
+    classname="particles-canvas"
+    params={{
+      particles: {
+        number: {
+          value: 35,
+          density: {
+            enable: true,
+            value_area: 900
+          }
+        },
+        shape: {
+          type: "star",
+          stroke: {
+            width: 10,
+            color: "rgb(118, 41, 190)"
+          }
+        }
+      }
+    }} />
     <Navbar />
     <Header />
     </>
