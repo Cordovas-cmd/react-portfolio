8ddb0a625a505c5600fa3ccc1dcc02c05b44b49f
Samuel Cordova <cordovas1337@gmail.com>
2021-07-02 19:28:31 -0500

    added some stuff

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,7 +16,7 @@ function Projects() {
                     <img src={process.env.PUBLIC_URL + "/assets/images/Notetaker.png"} className="card-img-top" alt="send-foodz-screenshot"/>
                         <div className="card-body">
                             <h5 className="card-title">Note Taker</h5>
-                            <p className="card-text">An application that generates random restaurants based on the inputted city. Also shows the weather of the city on search.</p>
+                            <p className="card-text">An app that takes notes for you and your busy schedule.</p>
                             <a role="button" href="github goes here" target="__blank" className="projbtn">Repository</a>
                             <a role="button" href="deployement goes here" target="__blank" className="projbtn">Webpage</a>
                         </div>
@@ -28,7 +28,7 @@ function Projects() {
                     <img src={process.env.PUBLIC_URL+"/assets/images/TeamProfile.png"} className="card-img-top" alt="stellar-sonic-screenshot" />
                         <div className="card-body">
                             <h5 className="card-title">Team Profile Gen</h5>
-                            <p className="card-text">A space to share your sound - an application where you can search for songs and add to your personal interactive playlist.</p>
+                            <p className="card-text">Creates a team profile for each member of your team.</p>
                             <a role="button" href="github goes here" target="__blank" className="projbtn  ">Repository</a>
                             <a href="deployement goes here" target="__blank" className="projbtn  ">Webpage</a>
                             
@@ -40,7 +40,7 @@ function Projects() {
                     <img src={process.env.PUBLIC_URL+"/assets/images/WorkdayScheduler.png"} className="card-img-top" alt="employee-directory-screenshot" />
                         <div className="card-body">
                             <h5 className="card-title">Work Day Scheduler</h5>
-                            <p className="card-text">Organizes employees' data in a table and includes a search bar to filter employees. Also includes a sort function that sorts data.</p>
+                            <p className="card-text">Creates a schedule for your busy workday.</p>
                             <a role="button" href="github goes here" target="__blank" className="projbtn  ">Repository</a>
                             <a href="deployement goes here" target="__blank" className="projbtn  ">Webpage</a>
                             
