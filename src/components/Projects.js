ac5932ab0cfaad5e58aab7005a3e997ac8a22f90
Samuel Cordova <cordovas1337@gmail.com>
2021-07-03 19:14:18 -0500

    added some links and descriptions

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,8 +17,8 @@ function Projects() {
                         <div className="card-body">
                             <h5 className="card-title">Note Taker</h5>
                             <p className="card-text">An app that takes notes for you and your busy schedule.</p>
-                            <a role="button" href="github goes here" target="__blank" className="projbtn">Repository</a>
-                            <a role="button" href="deployement goes here" target="__blank" className="projbtn">Webpage</a>
+                            <a role="button" href="https://github.com/Cordovas-cmd/note-taker" target="__blank" className="projbtn">Repository</a>
+                            <a role="button" href="https://calm-eyrie-82454.herokuapp.com/" target="__blank" className="projbtn">Webpage</a>
                         </div>
                 </div>
                 
@@ -30,7 +30,7 @@ function Projects() {
                         <div className="card-body">
                             <h5 className="card-title">Lorecraft</h5>
                             <p className="card-text">A WIP app that allows you to create an account, post, and view lore of some of your favorite games.</p>
-                            <a role="button" href="github goes here" target="__blank" className="projbtn">Repository</a>
+                            <a role="button" href="https://github.com/madison-vega/lore-craft" target="__blank" className="projbtn">Repository</a>
                             <a role="button" href="deployement goes here" target="__blank" className="projbtn">Webpage</a>
                         </div>
                 </div>
@@ -41,10 +41,10 @@ function Projects() {
                 <div className="card" >
                     <img src={process.env.PUBLIC_URL + "/assets/images/Notetaker.png"} className="card-img-top" alt="send-foodz-screenshot"/>
                         <div className="card-body">
-                            <h5 className="card-title">buget tracker</h5>
+                            <h5 className="card-title">Budget tracker</h5>
                             <p className="card-text">A digital budget tracker to easily keep track of funds and expenses.</p>
-                            <a role="button" href="github goes here" target="__blank" className="projbtn">Repository</a>
-                            <a role="button" href="deployement goes here" target="__blank" className="projbtn">Webpage</a>
+                            <a role="button" href="https://github.com/Cordovas-cmd/Online_offlineBudget_Tracker" target="__blank" className="projbtn">Repository</a>
+                            <a role="button" href="https://stormy-island-22575.herokuapp.com/" target="__blank" className="projbtn">Webpage</a>
                         </div>
                 </div>
                 
@@ -55,8 +55,8 @@ function Projects() {
                         <div className="card-body">
                             <h5 className="card-title">workout tracker</h5>
                             <p className="card-text">Tracks your workout routine so you can keep track and reach your goals :)</p>
-                            <a role="button" href="github goes here" target="__blank" className="projbtn  ">Repository</a>
-                            <a href="deployement goes here" target="__blank" className="projbtn  ">Webpage</a>
+                            <a role="button" href="https://github.com/Cordovas-cmd/NoSQL_WorkoutTracker" target="__blank" className="projbtn  ">Repository</a>
+                            <a href="https://intense-bayou-20743.herokuapp.com/?id=60ad2f4deef73700157507b5" target="__blank" className="projbtn  ">Webpage</a>
                             
                         </div>
                 </div>
